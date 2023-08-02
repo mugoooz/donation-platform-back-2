e8d5d2c5e1a2fd6ea1a05e8169e8f472efdfe918
Eric Mugo <mugoericpg20@gmail.com>
2023-08-02 22:22:27 +0300

    notification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+    def create
+      @user = User.find_by(email: params[:session][:email].downcase)
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        if @user.is_a?(Admin)
+          redirect_to admin_dashboard_path
+        elsif @user.is_a?(Charity)
+          redirect_to charity_dashboard_path
+        elsif @user.is_a?(Donor)
+          redirect_to donor_dashboard_path
+        else
+          # Handle unknown user type
+          flash[:alert] = "Unknown user type"
+          redirect_to root_path
+        end
+      else
+        flash.now[:alert] = 'Invalid email/password combination'
+        render 'new'
+      end
+    end
+  
+    def destroy
+      session.delete(:user_id)
+      @current_user = nil
+      redirect_to root_path
+    end
 end
